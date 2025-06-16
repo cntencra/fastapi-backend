74276ffa85b0820eb27d1a45bb79ade19ae29ab6
cntencra <cn10cra@gmail.com>
2025-06-16 16:36:39 +0100

    feat: TESTS UPDATED TO USE dbmANAGER CLASS

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,23 @@
 import os
 import pytest_asyncio
 
+from fastapi_backend.db.db_manager import DBManager
 from fastapi_backend.utils.path_utils import get_project_root
-from fastapi_backend.db.psql_connection import open_pool, close_pool
 from fastapi_backend.db.seeds.seed import run_seed
 from fastapi_backend.db.utils.seed_utils import load_seed_data
 
 test_data_path = get_project_root() / "fastapi_backend" / "db" / "data" / "test_data" / "seed_data.json"
 
-@pytest_asyncio.fixture(scope="session", autouse=True)
-async def setup_test_environment():
+
+
+@pytest_asyncio.fixture(scope="session")
+async def db_manager():
     os.environ["ENV"] = "test"
-    await open_pool()
+    manager = DBManager()
+    await manager.open_pool()
     seed_data = load_seed_data(test_data_path)
-    await run_seed(**seed_data.model_dump())
+    await run_seed(manager, **seed_data.model_dump())
     print("✅ Database seeded.")
-    yield
-    await close_pool()
+    yield manager
+    await manager.close_pool()
     print("🧹 Pool closed.")
