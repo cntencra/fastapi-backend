61fadc5afd102078dc03b749c7540518522e3d0b
cntencra <cn10cra@gmail.com>
2025-06-23 22:20:05 +0100

    feat: app bubble model

---
+++ fastapi_backend/app/models/bubble_models.py
@@ -0,0 +1,20 @@
+from typing import List
+
+from fastapi_backend.db import db_manager
+from fastapi_backend.app.schemas.db import BubbleDB
+
+class BubbleModel():
+    @staticmethod
+    async def get_all() -> List[BubbleDB]:
+        async with db_manager.get_conn() as conn:
+            async with conn.cursor() as cur:
+                await cur.execute("SELECT bubble_id, label, location_x, location_y FROM bubbles")
+                rows = await cur.fetchall()
+                return [
+                    BubbleDB(
+                        bubble_id=row[0],
+                        label=row[1],
+                        location_x=row[2],
+                        location_y=row[3]
+                    ) for row in rows
+                ]
