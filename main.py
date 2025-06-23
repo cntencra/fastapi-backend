690df469306402fcb81c21158127f1348b2bb793
cntencra <cn10cra@gmail.com>
2025-06-23 22:15:57 +0100

    fix: removed unnecessary comments from main

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ from fastapi_backend.app.routes import api
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # Startup
+  
     await db_manager.open_pool()
+    
     yield
-    # Shutdown
+  
     await db_manager.close_pool()
 
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(api.router, prefix="/api")
