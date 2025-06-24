752278b534af6685f3fccd303080d8c1d087f525
cntencra <cn10cra@gmail.com>
2025-06-24 12:55:44 +0100

    feat: use pytestmark = pytest.mark.anyio
    saves marking every test

--- tests/test_seed/test_seed.py
+++ tests/test_seed/test_seed.py
@@ -3,6 +3,7 @@ from fastapi_backend.db import DBManager
 from psycopg.rows import dict_row
 from psycopg import sql
 
+pytestmark = pytest.mark.anyio
 
 @pytest.mark.parametrize("table_name", [
     "bubbles",
@@ -12,7 +13,6 @@ from psycopg import sql
     "backgroundimages",
     "settings"
 ])
-@pytest.mark.anyio
 async def test_tables_exists(db_manager_fixture: DBManager, table_name: str):
     async with db_manager_fixture.get_conn() as conn:
         async with conn.cursor() as cur:
@@ -48,9 +48,7 @@ async def test_tables_exists(db_manager_fixture: DBManager, table_name: str):
     ,("settings","no_colors","integer")
     ,("settings","bubble_size_min","integer")
     ,("settings","bubble_size_max","integer")
-
 ])
-@pytest.mark.anyio
 async def test_table_column_data_type(db_manager_fixture: DBManager, table_name: str, column_name: str, data_type:str):
     async with db_manager_fixture.get_conn() as conn:
         async with conn.cursor(row_factory=dict_row) as cur:
@@ -75,7 +73,6 @@ async def test_table_column_data_type(db_manager_fixture: DBManager, table_name:
     ("backgroundimages", ["image_id", "image_url"],2),
     ("settings", ["setting_id", "no_colors", "bubble_size_min", "bubble_size_max"],1)
 ])
-@pytest.mark.anyio
 async def test_table_has_data(db_manager_fixture: DBManager, table_name: str, columns: list, test_data_length: int):
     async with db_manager_fixture.get_conn() as conn:
         async with conn.cursor(row_factory=dict_row) as cur:
