4c9911c5fd54c912560721e03340ab13ce9d7278
cntencra <cn10cra@gmail.com>
2025-06-24 12:54:07 +0100

    feat: remove visualisation schema

--- fastapi_backend/app/schemas/visualisation.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel  
-
-class BubbleVisualisation(BaseModel):
-    bubble_id: int
-    label: str
-    location_x: int
-    location_y: int
-    colour: str
-    size: int
