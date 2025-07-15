1f6e8d4e98ae09cb977286b1be748a091d9906ea
cntencra <cn10cra@gmail.com>
2025-07-15 16:31:38 +0100

    feat: routes get visits

--- fastapi_backend/app/routes/api.py
+++ fastapi_backend/app/routes/api.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter
 from fastapi_backend.app.controllers.bubble_controllers import BubbleController
-from fastapi_backend.app.schemas.db import BubbleDB
+from fastapi_backend.app.controllers.visit_controllers import VisitController   
+from fastapi_backend.app.schemas.db import BubbleDB, VisitDB
 
 router = APIRouter()
 
 @router.get("/bubbles", response_model=list[BubbleDB])   
 async def get_bubbles():
     return await BubbleController.list_bubbles()
+
+@router.get("/visits", response_model=list[VisitDB])
+async def get_visits():
+    return await VisitController.list_visits()
