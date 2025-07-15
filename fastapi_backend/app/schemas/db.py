12ddf39120ec3a187253e51d27a14b2e46cbbd30
cntencra <cn10cra@gmail.com>
2025-07-15 16:31:10 +0100

    feat: schemas get visits

--- fastapi_backend/app/schemas/db.py
+++ fastapi_backend/app/schemas/db.py
@@ -1,7 +1,14 @@
 from pydantic import BaseModel
+from typing import Optional, List
+from datetime import datetime
 
 class BubbleDB(BaseModel):
     bubble_id: int
     label: str
     location_x: int
     location_y: int
+
+class VisitDB(BaseModel):
+    visit_id: int
+    visit_name: Optional[str]
+    visit_date: datetime
