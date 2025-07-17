89cb39b67c9b94fea4aac11e04545748bca80359
cntencra <cn10cra@gmail.com>
2025-07-17 16:55:52 +0100

    fix: models change schema names

--- fastapi_backend/app/models/visit_models.py
+++ fastapi_backend/app/models/visit_models.py
@@ -1,16 +1,16 @@
 from typing import List
 from fastapi_backend.db import db_manager
-from fastapi_backend.app.schemas.db import VisitDB
+from fastapi_backend.app.schemas.database import Visit
 
 class VisitModel():
     @staticmethod
-    async def get_all() -> List[VisitDB]:
+    async def get_all() -> List[Visit]:
         async with db_manager.get_conn() as conn:
             async with conn.cursor() as cur:
                 await cur.execute("SELECT visit_id, visit_name, visit_date FROM visits")
                 rows = await cur.fetchall()
                 return [
-                    VisitDB(
+                    Visit(
                         visit_id=row[0],
                         visit_name=row[1],
                         visit_date=row[2]
