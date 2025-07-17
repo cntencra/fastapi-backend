d751140ec24f7d12bbf42e67c676cc2ecc37f89e
cntencra <cn10cra@gmail.com>
2025-07-17 16:55:05 +0100

    fix: controllers change schema names

--- fastapi_backend/app/controllers/bubble_controllers.py
+++ fastapi_backend/app/controllers/bubble_controllers.py
@@ -1,10 +1,10 @@
 from typing import List
 from fastapi_backend.app.models.bubble_models import BubbleModel
-from fastapi_backend.app.schemas.db import BubbleDB
+from fastapi_backend.app.schemas.database import Bubble
 
 class BubbleController:
     @staticmethod
-    async def list_bubbles() -> List[BubbleDB]:
+    async def list_bubbles() -> List[Bubble]:
         bubble_model = BubbleModel()
         bubbles  = await bubble_model.get_all()
         return bubbles
