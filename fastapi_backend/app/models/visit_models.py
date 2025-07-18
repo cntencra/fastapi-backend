95abba344b79efd66d9330cd223cb5bcc51f5c0d
cntencra <cn10cra@gmail.com>
2025-07-18 23:27:25 +0100

    fix: rewrote VisitModel

--- fastapi_backend/app/models/visit_models.py
+++ fastapi_backend/app/models/visit_models.py
@@ -1,18 +1,31 @@
 from typing import List
 from fastapi_backend.db import db_manager
-from fastapi_backend.app.schemas.database import Visit
+from fastapi_backend.app.schemas.database import  Data
 
 class VisitModel():
+
     @staticmethod
-    async def get_all() -> List[Visit]:
+    async def fetch_visits_by_pest_id(pest_id: int) -> List[int]:
+        async with db_manager.get_conn() as conn:
+             async with conn.cursor() as cur:
+                 await cur.execute("SELECT DISTINCT visit_id FROM data WHERE pest_id=%s",(pest_id,))
+                 rows = await cur.fetchall()
+                 return [
+                     (row[0]) for row in rows
+                 ]
+             
+    @staticmethod
+    async def fetch_visit_data(visit_id: int, pest_id: int) -> List[Data]:
         async with db_manager.get_conn() as conn:
             async with conn.cursor() as cur:
-                await cur.execute("SELECT visit_id, visit_name, visit_date FROM visits")
+                await cur.execute("SELECT data_id, bubble_id, visit_id, value, pest_id  FROM data WHERE visit_id=%s AND pest_id=%s",(visit_id, pest_id,))
                 rows = await cur.fetchall()
                 return [
-                    Visit(
-                        visit_id=row[0],
-                        visit_name=row[1],
-                        visit_date=row[2]
+                    Data(
+                        data_id=row[0],
+                        bubble_id=row[1],
+                        visit_id=row[2],
+                        value=row[3],
+                        pest_id=row[4]
                     ) for row in rows
                 ]
